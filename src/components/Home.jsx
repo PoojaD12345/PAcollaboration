1d4cc44533f662961a4ae4c5e6311b81c2d04783
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2022-06-24 13:38:00 +0530

    updated code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Home = () => {
+  const [count,setCount] = useState(0)
   return (
-    <div>Home</div>
+<>
+<div>Home</div>
+<h1>count:{count}</h1>;
+<button onClick={()=>{setCount=count+1}}>inc</button>
+<button onClick={()=>{setCount=count-1}}>dec</button>
+</>
   )
 }
 
